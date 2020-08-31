c7111be3739654751e796af28c254441d53b5f00
anaideia1 <ukr.shevolex@gmail.com>
2020-08-31 22:45:08 +0300

    updated

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostsListView.as_view(), name="post_list"),\
-    path('reversed/', views.PostsReversedListView.as_view(), name="post_reversed_list"),
-    path('newest', views.PostsNewestListView.as_view(), name="post_newest"),
-    path('<int:pk>/', views.PostDetailView.as_view(), name="post_detail"),
+    path('', views.PostsListView.as_view(), name="post_list"),
+    path('newest/', views.PostsNewestListView.as_view(), name="post_newest"),
     path('new/', views.PostCreateView.as_view(), name="post_new"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from core.models import Post
 
@@ -11,12 +12,21 @@ class PostsListView(generic.ListView):
     template_name = 'post_list.html'
     context_object_name = 'posts'
 
+    def get_ordering(self):
+        order = self.request.GET.get('order', 'asc')
+        return order
 
-class PostsReversedListView(generic.ListView):
-    model = Post
-    queryset = Post.objects.all()[::-1]
-    template_name = 'post_reversed_list.html'
-    context_object_name = 'reversed_posts'
+    def get_last_param(self):
+        last = self.request.GET.get("last", "day")
+        return last
+
+    def get_queryset(self):
+        order = self.get_ordering()
+        last = self.get_last_param()
+        if order == "asc":
+            return Post.objects.all().order_by("publish")
+        elif order == "desc":
+            return Post.objects.all().order_by("-publish")
 
 
 class PostsNewestListView(generic.ListView):
@@ -26,13 +36,8 @@ class PostsNewestListView(generic.ListView):
     context_object_name = 'posts'
 
 
-class PostDetailView(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-    context_object_name = 'post'
-
-
-class PostCreateView(generic.edit.CreateView):
+class PostCreateView(generic.CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'body']
+    success_url = reverse_lazy("post_list")
