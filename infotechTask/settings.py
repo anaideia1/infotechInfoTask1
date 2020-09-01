ee195b1c6208ec665d44dc52dbfbe36bc750d5e0
anaideia1 <ukr.shevolex@gmail.com>
2020-09-01 15:42:20 +0300

    fix

--- infotechTask/settings.py
+++ infotechTask/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o^yk2*i=7)h246!d3=appgx$y0s72+s*uwwlo1zdqt9ae6_e^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['infotech-intro-task.herokuapp.com', 'localhost']
 
